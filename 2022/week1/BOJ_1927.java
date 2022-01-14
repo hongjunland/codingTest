c6297656e3680a2b1e51e3aba78d30252cf8916c
이홍준 <zunza96@naver.com>
2022-01-14 23:49:31 +0900

    post BOJ_1946.java

---
+++ 2022/week1/BOJ_1927.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_1927 {
+    static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+    }
+}
