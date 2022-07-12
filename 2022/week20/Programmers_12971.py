6e5f835cc53b959a57c1c1b330e8995e8121f4d8
이홍준 <zunza96@naver.com>
2022-07-12 20:14:01 +0900

    post

--- 2022/week20/Programmers_12971.py
+++ 2022/week20/Programmers_12971.py
@@ -1,6 +1,9 @@
 def solution(sticker):
-    answer = 0
     n = len(sticker)
+    if n == 2:
+        return max(sticker[0], sticker[1])
+    if n == 1:
+        return sticker[0]
     dp1 = [0] * n
     dp2 = [0] * n
     dp1[0] = sticker[0]
@@ -10,6 +13,4 @@ def solution(sticker):
     for i in range(2,n-1):
         dp1[i] = max(dp1[i-1], dp1[i-2]+ sticker[i])
         dp2[i+1] = max(dp2[i], dp2[i-1] + sticker[i+1])
-    print(dp1)     
-    print(dp2)
     return max(dp1[-2], dp2[-1])
