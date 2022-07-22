7e6237a8aaea4111c5c471dbe06d95cd67e4dc35
이홍준 <zunza96@naver.com>
2022-07-22 23:53:14 +0900

    post

--- 2022/week22/Programmers_60057.py
+++ 2022/week22/Programmers_60057.py
@@ -1,3 +1,21 @@
 def solution(s):
-    answer = 0
-    return answer
+    answer = s
+    msg = list(s)
+    for i in range(1, len(msg)+1):
+        if len(s) % i != 0:
+            continue
+        current = ''
+        buffer = [msg[:i], 1]
+        for j in range(i,len(msg),i):
+            if ''.join(msg[j:j+i]) == buffer[0]:
+                buffer[1] += 1
+                continue
+            if buffer[1] > 1:
+                current += str(buffer[1])
+            current += ''.join(buffer[0])
+            buffer = [''.join(msg[j:j+i]),1]
+        print(current)
+        if len(current) < len(answer):
+            answer = current
+    print(answer)
+    return len(answer)
