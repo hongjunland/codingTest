900965c5a76d9ac6061fd2fa08d54bb8b5f10907
hongjunland <zumza96@gmail.com>
2023-02-08 20:13:54 +0900

    Feat: update

---
+++ 2023/week2/BOJ_231.py
@@ -0,0 +1,16 @@
+"""
+input : 216
+output: 196
+"""
+number_input = int(input())
+result = 0
+for candidate_number in range(number_input):
+    current_number = candidate_number
+    digit_sum = 0
+    while current_number > 0:
+        digit_sum += current_number % 10
+        current_number //= 10
+    if digit_sum + candidate_number == number_input:
+        result = candidate_number
+        break
+print(result)
